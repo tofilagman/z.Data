424dfa91c8516b978f62254348909ffedc376db8
tofi lagman <tofilagman@gmail.com>
2017-09-26 17:16:36 +0800

    Add project files.

---
+++ Args.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z.Data
+{
+    public class Args : Dictionary<string, object>
+    {
+        public Args(string ColumnName, object Value)
+        {
+            base.Add(ColumnName, Value);
+        }
+
+        public new Args Add(string column, object value)
+        {
+            base.Add(column, value);
+            return this;
+        }
+    }
+}
---
+++ SBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z.Data
+{
+    public class SBuilder : IDisposable
+    {
+        private StringBuilder sb = new StringBuilder();
+
+        public void Append(object str)
+        {
+            sb.Append(str);
+        }
+
+        public void Append(string str, params object[] args)
+        {
+            this.Append(string.Format(str, args));
+        }
+
+        public void AppendLine(string str)
+        {
+            sb.AppendLine(str);
+        }
+
+        public void AppendLine(string str, params object[] args)
+        {
+            this.AppendLine(string.Format(str, args));
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+
+        public void Dispose()
+        {
+            sb = null;
+            GC.Collect();
+        }
+    }
+}
